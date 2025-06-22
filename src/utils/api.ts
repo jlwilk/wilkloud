b1038a14b398465007d37301445bb49441dbc1a3
Jason Wilkin <jason@wilk.in>
2025-06-22 17:23:19 -0600

    adds new favicon and a little name cleanup

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ const apiHost = window.location.hostname
 let apiUrl = import.meta.env.VITE_API_URL
 
 if (apiHost === 'localhost' || apiHost === '127.0.0.1') {
-  apiUrl = import.meta.env.VITE_API_LOCAL_URL || 'http://localhost:3000'
+  apiUrl = import.meta.env.VITE_API_LOCAL_URL || 'http://localhost:8000'
 }
 
 export default apiUrl
